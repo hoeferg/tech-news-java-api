2ce473d0634025b3bce4052516876a7d73336d7d
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-31 15:50:49 -0700

    routes are completed

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/technewsjavaapi/controller/TechNewController.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/technewsjavaapi/controller/TechNewController.java
@@ -88,6 +88,85 @@ public class TechNewsController {
             request.getSession().setAttribute("SESSION_USER", sessionUser);
 
             return "redirect:/dashboard";
+
+            @PostMapping("/posts")
+            public String addPostDashboardPage(@ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+                if ((post.getTitle().equals(null) || post.getTitle().isEmpty()) || (post.getPostUrl().equals(null) || post.getPostUrl().isEmpty())) {
+                    return "redirect:/dashboardEmptyTitleAndLink";
+                }
+
+                if (request.getSession(false) == null) {
+                    return "redirect:/login";
+                } else {
+                    User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                    post.setUserId(sessionUser.getId());
+                    postRepository.save(post);
+
+                    return "redirect:/dashboard";
+                }
+            }
+            @PostMapping("/posts/{id}")
+            public String updatePostDashboardPage(@PathVariable int id, @ModelAttribute Post post, Model model, HttpServletRequest request) {
+
+                if (request.getSession(false) == null) {
+                    model.addAttribute("user", new User());
+                    return "redirect/dashboard";
+                } else {
+                    Post tempPost = postRepository.getById(id);
+                    tempPost.setTitle(post.getTitle());
+                    postRepository.save(tempPost);
+
+                    return "redirect:/dashboard";
+                }
+            }
+            @PostMapping("/comments")
+            public String createCommentCommentsPage(@ModelAttribute Comment comment, Model model, HttpServletRequest request) {
+
+                if (comment.getCommentText().isEmpty() || comment.getCommentText().equals(null)) {
+                    return "redirect:/singlePostEmptyComment/" + comment.getPostId();
+                } else {
+                    if (request.getSession(false) != null) {
+                        User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                        comment.setUserId(sessionUser.getId());
+                        commentRepository.save(comment);
+                        return "redirect:/post/" + comment.getPostId();
+                    } else {
+                        return "login";
+                    }
+                }
+            }
+            @PostMapping("/comments/edit")
+            public String createCommentEditPage(@ModelAttribute Comment comment, HttpServletRequest request) {
+
+                if (comment.getCommentText().equals("") || comment.getCommentText().equals(null)) {
+                    return "redirect:/editPostEmptyComment/" + comment.getPostId();
+                } else {
+                    if (request.getSession(false) != null) {
+                        User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                        comment.setUserId(sessionUser.getId());
+                        commentRepository.save(comment);
+
+                        return "redirect:/dashboard/edit/" + comment.getPostId();
+                    } else {
+                        return "redirect:/login";
+                    }
+                }
+
+            }
+            @PutMapping("/posts/upvote")
+            public void addVoteCommentsPage(@RequestBody Vote vote, HttpServletRequest request, HttpServletResponse response) {
+
+                if (request.getSession(false) != null) {
+                    Post returnPost = null;
+                    User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+                    vote.setUserId(sessionUser.getId());
+                    voteRepository.save(vote);
+
+                    returnPost = postRepository.getById(vote.getPostId());
+                    returnPost.setVoteCount(voteRepository.countVotesByPostId(vote.getPostId()));
+                }
+            }
         }
     }
 
